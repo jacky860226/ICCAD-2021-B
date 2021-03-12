75520025ed4a43892abfae2549c55dd017c62fe8
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-03-12 16:54:05 +0800

    add parse functions

--- src/lib/cell_move_router/Parser.cpp
+++ src/lib/cell_move_router/Parser.cpp
@@ -22,6 +22,120 @@ Parser::parseGGridBoundaryIdx(std::istream &input) {
   return std::make_tuple(RowBeginIdx, ColBeginIdx, RowEndIdx, ColEndIdx);
 }
 
+std::vector<RawData::Layer> Parser::parseLayers(std::istream &input) {
+  std::string buff;
+  input >> buff;
+  assert(buff == "NumLayer");
+  int LayerCount = 0;
+  input >> LayerCount;
+  std::vector<RawData::Layer> Layers;
+  for (int i = 0; i < LayerCount; i++) {
+    input >> buff;
+    assert(buff == "Lay");
+    std::string LayerName;
+    int Idx;
+    char RoutingDir;
+    int DefaultSupplyOfOneGGrid;
+    double PowerFactor;
+    input >> LayerName >> Idx >> RoutingDir >> DefaultSupplyOfOneGGrid >>
+        PowerFactor;
+    Layers.emplace_back(std::move(LayerName), Idx, RoutingDir,
+                        DefaultSupplyOfOneGGrid, PowerFactor);
+  }
+  return Layers;
+}
+
+std::vector<RawData::NonDefaultSupplyGGrid>
+Parser::parseNonDefaultSupplyGGrids(std::istream &input) {
+  std::string buff;
+  input >> buff;
+  assert(buff == "NumNonDefaultSupplyGGrid");
+  int NumNonDefaultSupplyGGrid = 0;
+  input >> NumNonDefaultSupplyGGrid;
+  std::vector<RawData::NonDefaultSupplyGGrid> NonDefaultSupplyGGrids;
+  for (int i = 0; i < NumNonDefaultSupplyGGrid; i++) {
+    int RowIdx, ColIdx, LayIdx;
+    int IncrOrDecrValue;
+    input >> RowIdx >> ColIdx >> LayIdx >> IncrOrDecrValue;
+    NonDefaultSupplyGGrids.emplace_back(RowIdx, ColIdx, LayIdx,
+                                        IncrOrDecrValue);
+  }
+  return NonDefaultSupplyGGrids;
+}
+
+RawData::MasterCell::Pin parsePin(std::istream &input) {
+  std::string buff;
+  input >> buff;
+  assert(buff == "Pin");
+  std::string PinName;
+  std::string PinLayer;
+  input >> PinName >> PinLayer;
+  return RawData::MasterCell::Pin(std::move(PinName), std::move(PinLayer));
+}
+
+RawData::MasterCell::Blkg parseBlkg(std::istream &input) {
+  std::string buff;
+  input >> buff;
+  assert(buff == "Blkg");
+  std::string BlockageName;
+  std::string BlockageLayer;
+  int Demand;
+  input >> BlockageName >> BlockageLayer >> Demand;
+  return RawData::MasterCell::Blkg(std::move(BlockageName),
+                                   std::move(BlockageLayer), Demand);
+}
+
+std::vector<RawData::MasterCell> Parser::parseMasterCells(std::istream &input) {
+  std::string buff;
+  input >> buff;
+  assert(buff == "NumMasterCell");
+  int NumMasterCell = 0;
+  input >> NumMasterCell;
+  std::vector<RawData::MasterCell> MasterCells;
+  for (int i = 0; i < NumMasterCell; i++) {
+    input >> buff;
+    assert(buff == "MasterCell");
+    std::string MasterCellName;
+    int PinCount = 0;
+    int BlkgCount = 0;
+    input >> MasterCellName >> PinCount >> BlkgCount;
+    std::vector<RawData::MasterCell::Pin> Pins;
+    std::vector<RawData::MasterCell::Blkg> Blkgs;
+    for (int j = 0; j < PinCount; j++) {
+      Pins.emplace_back(parsePin(input));
+    }
+    for (int j = 0; j < BlkgCount; j++) {
+      Blkgs.emplace_back(parseBlkg(input));
+    }
+    MasterCells.emplace_back(std::move(MasterCellName), std::move(Pins),
+                             std::move(Blkgs));
+  }
+  return MasterCells;
+}
+
+std::vector<RawData::CellInst> Parser::parseCellInsts(std::istream &input) {
+  std::string buff;
+  input >> buff;
+  assert(buff == "NumCellInst");
+  int CellInstCount;
+  input >> CellInstCount;
+  std::vector<RawData::CellInst> CellInsts;
+  for (int i = 0; i < CellInstCount; i++) {
+    input >> buff;
+    assert(buff == "CellInst");
+    std::string InstName;
+    std::string MasterCellName;
+    int GGridRowIdx;
+    int GGridColIdx;
+    std::string MovableCstr;
+    input >> InstName >> MasterCellName >> GGridRowIdx >> GGridColIdx >>
+        MovableCstr;
+    CellInsts.emplace_back(std::move(InstName), std::move(MasterCellName),
+                           GGridRowIdx, GGridColIdx, MovableCstr == "Movable");
+  }
+  return CellInsts;
+}
+
 // TODO: other parse function
 
 std::unique_ptr<RawData::Input> Parser::parse(std::istream &input) {
@@ -31,11 +145,15 @@ std::unique_ptr<RawData::Input> Parser::parse(std::istream &input) {
   std::tie(RowBeginIdx, ColBeginIdx, RowEndIdx, ColEndIdx) =
       parseGGridBoundaryIdx(input);
 
-  // TODO: call other parse function
-  std::vector<RawData::Layer> Layers;
-  std::vector<RawData::NonDefaultSupplyGGrid> NonDefaultSupplyGGrids;
-  std::vector<RawData::MasterCell> MasterCells;
-  std::vector<RawData::CellInst> CellInsts;
+  std::vector<RawData::Layer> Layers = parseLayers(input);
+
+  std::vector<RawData::NonDefaultSupplyGGrid> NonDefaultSupplyGGrids =
+      parseNonDefaultSupplyGGrids(input);
+
+  std::vector<RawData::MasterCell> MasterCells = parseMasterCells(input);
+
+  std::vector<RawData::CellInst> CellInsts = parseCellInsts(input);
+
   std::vector<RawData::Net> Nets;
   std::vector<RawData::VoltageArea> VoltageAreas;
   std::vector<RawData::Route> Routes;
--- src/lib/cell_move_router/RawData/Datas.cpp
+++ src/lib/cell_move_router/RawData/Datas.cpp
@@ -1,10 +1,81 @@
 #include "cell_move_router/RawData/Datas.hpp"
 namespace cell_move_router {
 namespace RawData {
-void Layer::to_ostream(std::ostream &out) const {}
-void NonDefaultSupplyGGrid::to_ostream(std::ostream &out) const {}
-void MasterCell::to_ostream(std::ostream &out) const {}
-void CellInst::to_ostream(std::ostream &out) const {}
+
+Layer::Layer(std::string &&LayerName, const int Idx, const char RoutingDir,
+             const int DefaultSupplyOfOneGGrid, const double PowerFactor)
+    : LayerName(LayerName), Idx(Idx), RoutingDir(RoutingDir),
+      DefaultSupplyOfOneGGrid(DefaultSupplyOfOneGGrid),
+      PowerFactor(PowerFactor) {}
+void Layer::to_ostream(std::ostream &out) const {
+  out << "Lay " << LayerName << ' ' << Idx << ' ' << RoutingDir << ' '
+      << DefaultSupplyOfOneGGrid << ' ' << PowerFactor << '\n';
+}
+
+NonDefaultSupplyGGrid::NonDefaultSupplyGGrid(const int RowIdx, const int ColIdx,
+                                             const int LayIdx,
+                                             const int IncrOrDecrValue)
+    : RowIdx(RowIdx), ColIdx(ColIdx), LayIdx(LayIdx),
+      IncrOrDecrValue(IncrOrDecrValue) {}
+
+void NonDefaultSupplyGGrid::to_ostream(std::ostream &out) const {
+  auto OutputWithSign = [&](int x) {
+    std::string Ans;
+    if (x >= 0)
+      Ans = "+";
+    Ans += std::to_string(x);
+    return Ans;
+  };
+  out << RowIdx << ' ' << ColIdx << ' ' << LayIdx << ' '
+      << OutputWithSign(IncrOrDecrValue) << '\n';
+}
+
+MasterCell::MasterCell(std::string &&MasterCellName, std::vector<Pin> &&Pins,
+                       std::vector<Blkg> &&Blkgs)
+    : MasterCellName(MasterCellName), Pins(Pins), Blkgs(Blkgs) {}
+
+void MasterCell::to_ostream(std::ostream &out) const {
+  out << "MasterCell " << MasterCellName << ' ' << Pins.size() << ' '
+      << Blkgs.size();
+  for (const auto &pin : Pins) {
+    pin.to_ostream(out);
+  }
+  for (const auto &blkg : Blkgs) {
+    blkg.to_ostream(out);
+  }
+}
+
+MasterCell::Pin::Pin(std::string &&PinName, std::string &&PinLayer)
+    : PinName(PinName), PinLayer(PinLayer) {}
+
+void MasterCell::Pin::to_ostream(std::ostream &out) const {
+  out << "Pin " << PinName << ' ' << PinLayer << '\n';
+}
+
+MasterCell::Blkg::Blkg(std::string &&BlockageName, std::string &&BlockageLayer,
+                       const int Demand)
+    : BlockageName(BlockageName), BlockageLayer(BlockageLayer), Demand(Demand) {
+}
+
+void MasterCell::Blkg::to_ostream(std::ostream &out) const {
+  out << "Blkg " << BlockageName << ' ' << BlockageLayer << ' ' << Demand
+      << '\n';
+}
+
+CellInst::CellInst(std::string &&InstName, std::string &&MasterCellName,
+                   const int GGridRowIdx, const int GGridColIdx,
+                   const bool IsMovable)
+    : InstName(InstName), MasterCellName(MasterCellName),
+      GGridRowIdx(GGridRowIdx), GGridColIdx(GGridColIdx), IsMovable(IsMovable) {
+}
+
+void CellInst::to_ostream(std::ostream &out) const {
+  out << "CellInst" << InstName << ' ' << MasterCellName << ' ' << GGridRowIdx
+      << ' ' << GGridColIdx << ' ' << (IsMovable ? "Movable" : "Fixed") << '\n';
+}
+
+// TODO
+
 void Net::to_ostream(std::ostream &out) const {}
 void VoltageArea::to_ostream(std::ostream &out) const {}
 void Route::to_ostream(std::ostream &out) const {}
