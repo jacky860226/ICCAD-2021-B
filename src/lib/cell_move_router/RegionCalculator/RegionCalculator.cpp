f0960dffceafab664d23747a10cdbd558d8a22a5
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-07-05 18:25:16 +0800

    finish RegionCalculator
    finish RegionCalculator.hpp
    finish RegionCalculator.cpp

---
+++ src/lib/cell_move_router/RegionCalculator/RegionCalculator.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <cell_move_router/RegionCalculator/RegionCalculator.hpp>
+namespace cell_move_router {
+namespace RegionCalculator {
+
+std::pair<int, int> RegionCalculator::getMedium(std::vector<int> &V) {
+  int L = (V.size() - 1) >> 1;
+  int R = V.size() >> 1;
+  std::nth_element(V.begin(), V.begin() + R, V.end());
+  std::nth_element(V.begin(), V.begin() + L, V.begin() + R);
+  return std::make_pair(V[L], V[R]);
+}
+
+std::tuple<int, int, int, int>
+RegionCalculator::getRegion(const Input::Processed::Net *NetPtr) {
+  std::vector<int> Rows, Cols;
+  for (const auto &Pin : NetPtr->getPins()) {
+    Rows.emplace_back(Pin.getInst()->getGGridRowIdx());
+    Cols.emplace_back(Pin.getInst()->getGGridColIdx());
+  }
+  auto RowCoord = getMedium(Rows);
+  auto ColCoord = getMedium(Cols);
+  return make_tuple(RowCoord.first, RowCoord.second, ColCoord.first,
+                    ColCoord.second);
+}
+
+} // namespace RegionCalculator
+} // namespace cell_move_router
