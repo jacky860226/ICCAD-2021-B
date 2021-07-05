aadd30f3a81eab691f890a2027454ebaa7d04b64
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-07-05 19:16:19 +0800

    revise RegionCalculator Structure

--- src/include/cell_move_router/RegionCalculator/RegionCalculator.hpp
+++ src/include/cell_move_router/RegionCalculator/RegionCalculator.hpp
@@ -1,19 +1,6 @@
-#include <cell_move_router/Input/Processed/Datas.hpp>
-#include <tuple>
-#include <utility>
+#include <cell_move_router/RegionCalculator/OptimalRegion.hpp>
 namespace cell_move_router {
 namespace RegionCalculator {
-
-class RegionCalculator {
-private:
-  int kth_element(std::vector<int> &, const int, const int, const int);
-  std::pair<int, int> getMedium(std::vector<int> &);
-
-public:
-  RegionCalculator() {}
-  // {BeginRowIdx, EndRowIdx, BeginColIdx, EndColIdx}
-  std::tuple<int, int, int, int> getRegion(const Input::Processed::Net *);
-};
-
+class RegionCalculator : public OptimalRegion {}
 } // namespace RegionCalculator
 } // namespace cell_move_router
