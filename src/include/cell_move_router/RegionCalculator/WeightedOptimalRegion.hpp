9f93c885f829127cfcaf5ab995aaa266359ead5f
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-07-06 20:08:01 +0800

    fix weighted optimal region

--- src/include/cell_move_router/RegionCalculator/WeightedOptimalRegion.hpp
+++ src/include/cell_move_router/RegionCalculator/WeightedOptimalRegion.hpp
@@ -2,11 +2,13 @@
 namespace cell_move_router {
 namespace RegionCalculator {
 
-class OptimalRegion : RegionCalculator {
-  std::pair<int, int> getMedium(std::vector<int> &V);
+class WeightedOptimalRegion : RegionCalculator {
+  int weightElement(std::vector<std::pair<int, double>> &V, const int L,
+                    const int R, double W);
+  std::pair<int, int> getWeightedMedium(std::vector<std::pair<int, double>> &V);
 
 public:
-  OptimalRegion() {}
+  WeightedOptimalRegion() {}
   std::tuple<int, int, int, int>
   getRegion(const Input::Processed::Net *Net) override;
 };
---
+++ src/lib/cell_move_router/RegionCalculator/WeightedOptimalRegion.cpp
@@ -0,0 +1,50 @@
+#include "cell_move_router/RegionCalculator/WeightedOptimalRegion.hpp"
+#include <algorithm>
+namespace cell_move_router {
+namespace RegionCalculator {
+
+int WeightedOptimalRegion::weightElement(std::vector<std::pair<int, double>> &V,
+                                         const int L, const int R, double W) {
+  int Pivot = rand() % (R - L) + L;
+  std::swap(V[Pivot], V[L]);
+  int j = L;
+  for (int i = L + 1; i < R; ++i) {
+    if (V[i].first < V[L].first)
+      std::swap(V[++j], V[i]);
+  }
+  std::swap(V[j], V[L]);
+  double Sum = 0;
+  for (int i = L; i < j; ++i)
+    Sum += V[i].second;
+  if (Sum > W)
+    return weightElement(V, L, j, W);
+  Sum += V[j].second;
+  if (Sum > W)
+    return V[j].first;
+  return weightElement(V, j + 1, R, W - Sum);
+}
+
+std::pair<int, int> WeightedOptimalRegion::getWeightedMedium(
+    std::vector<std::pair<int, double>> &V) {
+  double Sum = 0;
+  for (const auto &P : V)
+    Sum += P.second;
+  int Ret = weightElement(V, 0, V.size(), Sum * 0.5);
+  return {Ret, Ret};
+}
+
+std::tuple<int, int, int, int>
+WeightedOptimalRegion::getRegion(const Input::Processed::Net *Net) {
+  std::vector<std::pair<int, double>> Rows, Cols;
+  for (const auto &Pin : Net->getPins()) {
+    double Weight = Pin.getMasterPin()->getPinLayer()->getPowerFactor();
+    Rows.emplace_back(Pin.getInst()->getGGridRowIdx(), Weight);
+    Cols.emplace_back(Pin.getInst()->getGGridColIdx(), Weight);
+  }
+  auto RowCoord = getWeightedMedium(Rows);
+  auto ColCoord = getWeightedMedium(Cols);
+  return {RowCoord.first, RowCoord.second, ColCoord.first, ColCoord.second};
+}
+
+} // namespace RegionCalculator
+} // namespace cell_move_router
