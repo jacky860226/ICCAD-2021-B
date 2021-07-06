074ae2744fd7c462c949f2cccf5e46961e7d7381
104000045 <jacky860226@yahoo.com.tw>
2021-07-06 16:55:15 +0800

    fix RegionCalculator structure

--- src/include/cell_move_router/RegionCalculator/OptimalRegion.hpp
+++ src/include/cell_move_router/RegionCalculator/OptimalRegion.hpp
@@ -1,18 +1,14 @@
-#include <cell_move_router/Input/Processed/Datas.hpp>
-#include <tuple>
-#include <utility>
+#include "cell_move_router/RegionCalculator/RegionCalculator.hpp"
 namespace cell_move_router {
 namespace RegionCalculator {
 
-class OptimalRegion {
-private:
-  int kth_element(std::vector<int> &, const int, const int, const int);
-  std::pair<int, int> getMedium(std::vector<int> &);
+class OptimalRegion : RegionCalculator {
+  std::pair<int, int> getMedium(std::vector<int> &V);
 
 public:
   OptimalRegion() {}
-  // {BeginRowIdx, EndRowIdx, BeginColIdx, EndColIdx}
-  std::tuple<int, int, int, int> getRegion(const Input::Processed::Net *);
+  std::tuple<int, int, int, int>
+  getRegion(const Input::Processed::Net *Net) override;
 };
 
 } // namespace RegionCalculator
