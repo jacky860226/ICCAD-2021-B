d2d5d9951bb6df990776693d7d49b7e16c3a859c
104000045 <jacky860226@yahoo.com.tw>
2021-07-27 14:00:58 +0800

    [Router] use BondaryBuilder

--- src/include/cell_move_router/Router/GraphApproxRouter.hpp
+++ src/include/cell_move_router/Router/GraphApproxRouter.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Third/SteinerTree2/Solver.hpp"
+#include "cell_move_router/Router/BondaryBuilder.hpp"
 #include "cell_move_router/Router/RouterBase.hpp"
 
 namespace cell_move_router {
@@ -7,28 +8,18 @@ namespace Router {
 
 class RoutingGraphManager {
   steiner_tree::UndirectedGraph<long long> G;
-  cell_move_router::CoordinateCodec<unsigned long long> Codec;
   Grid::GridManager *GridManager;
-  const Input::Processed::Net *Net;
-  std::unordered_map<size_t, int> PinMinLayer;
-  std::unordered_set<size_t> Terminals;
-  int MinLayerIdx, MaxLayerIdx;
-  int MaxR, MinR;
-  int MaxC, MinC;
+  const RoutingBondaryInfo *BondaryInfo;
 
 public:
-  RoutingGraphManager() : Codec({0, 0, 0}){};
+  RoutingGraphManager(Grid::GridManager *GridManager)
+      : GridManager(GridManager){};
+  void setBondaryInfo(const RoutingBondaryInfo *BI) { BondaryInfo = BI; }
   const steiner_tree::UndirectedGraph<long long> &getGraph() const { return G; }
-  const std::unordered_set<size_t> &getTerminals() const { return Terminals; }
-  void setGraphInfo(Grid::GridManager *_GridManager,
-                    const Input::Processed::Net *_Net,
-                    const std::vector<Input::Processed::Route> &OriginRoute);
-  std::vector<Input::Processed::Route> createTerminalsAndRouteUnderMinLayer();
   void createGraph(const std::vector<long long> &LayerFactor,
                    const std::vector<char> &LayerDir);
   std::vector<Input::Processed::Route>
-  createFinalRoute(const std::vector<size_t> &Eids,
-                   std::vector<Input::Processed::Route> &&RouteUnderMinLayer);
+  createFinalRoute(const std::vector<size_t> &Eids, const Input::Processed::Net *Net);
 };
 
 class GraphApproxRouter : public RouterBase {
@@ -36,7 +27,7 @@ class GraphApproxRouter : public RouterBase {
 
 public:
   GraphApproxRouter(Grid::GridManager *GridManagerPtr)
-      : RouterBase(GridManagerPtr) {}
+      : RouterBase(GridManagerPtr), RGM(GridManagerPtr) {}
 
   std::pair<std::vector<Input::Processed::Route>, bool> singleNetRoute(
       const Input::Processed::Net *Net,
--- src/lib/cell_move_router/Router/GraphApproxRouter.cpp
+++ src/lib/cell_move_router/Router/GraphApproxRouter.cpp
@@ -4,122 +4,42 @@
 namespace cell_move_router {
 namespace Router {
 
-void RoutingGraphManager::setGraphInfo(
-    Grid::GridManager *_GridManager, const Input::Processed::Net *_Net,
-    const std::vector<Input::Processed::Route> &OriginRoute) {
-  GridManager = _GridManager;
-  Net = _Net;
-
-  const auto &InputPtr = GridManager->getInputPtr();
-  int RowBeginIdx = InputPtr->getRowBeginIdx();
-  int ColBeginIdx = InputPtr->getColBeginIdx();
-  int RowEndIdx = InputPtr->getRowEndIdx();
-  int ColEndIdx = InputPtr->getColEndIdx();
-  int LayerSz = InputPtr->getLayers().size();
-  auto MinRoutingLayConstraint = Net->getMinRoutingLayConstraint();
-
-  MaxR = RowBeginIdx;
-  MinR = RowEndIdx;
-  MaxC = ColBeginIdx;
-  MinC = ColEndIdx;
-  MinLayerIdx = LayerSz;
-  MaxLayerIdx = MinRoutingLayConstraint->getIdx();
-
-  for (const auto &Pin : Net->getPins()) {
-    int Row = 0, Col = 0;
-    std::tie(Row, Col) = GridManager->getCellCoordinate(Pin.getInst());
-    int Layer = Pin.getMasterPin()->getPinLayer()->getIdx();
-    MinR = std::min(MinR, Row);
-    MaxR = std::max(MaxR, Row);
-    MinC = std::min(MinC, Col);
-    MaxC = std::max(MaxC, Col);
-    MinLayerIdx = std::min(MinLayerIdx, Layer);
-    MaxLayerIdx = std::max(MaxLayerIdx, Layer);
-  }
-
-  for (const auto &Route : OriginRoute) {
-    MinR = std::min(MinR, Route.getSRowIdx());
-    MaxR = std::max(MaxR, Route.getERowIdx());
-    MinC = std::min(MinC, Route.getSColIdx());
-    MaxC = std::max(MaxC, Route.getEColIdx());
-    MinLayerIdx = std::min(MinLayerIdx, Route.getSLayIdx());
-    MaxLayerIdx = std::max(MaxLayerIdx, Route.getELayIdx());
-  }
-
-  MinR = std::max(MinR - 5, RowBeginIdx);
-  MaxR = std::min(MaxR + 5, RowEndIdx);
-  MinC = std::max(MinC - 5, ColBeginIdx);
-  MaxC = std::min(MaxC + 5, ColEndIdx);
-  MinLayerIdx = std::max(MinLayerIdx - 1, MinRoutingLayConstraint->getIdx());
-  MaxLayerIdx = std::min(MaxLayerIdx + 1, LayerSz);
-
-  Codec = cell_move_router::CoordinateCodec<unsigned long long>(
-      {(unsigned long long)(MaxR - MinR + 1),
-       (unsigned long long)(MaxC - MinC + 1),
-       (unsigned long long)(MaxLayerIdx - MinLayerIdx + 1)});
-}
-std::vector<Input::Processed::Route>
-RoutingGraphManager::createTerminalsAndRouteUnderMinLayer() {
-  PinMinLayer.clear();
-  Terminals.clear();
-  for (const auto &Pin : Net->getPins()) {
-    int Row = 0, Col = 0;
-    std::tie(Row, Col) = GridManager->getCellCoordinate(Pin.getInst());
-    int Layer = Pin.getMasterPin()->getPinLayer()->getIdx();
-    size_t Coord = Codec.encode(
-        {(unsigned long long)(Row - MinR), (unsigned long long)(Col - MinC),
-         (unsigned long long)(std::max(Layer, MinLayerIdx) - MinLayerIdx)});
-    Terminals.emplace(Coord);
-    if (Layer < MinLayerIdx) {
-      size_t Coord = Codec.encode({(unsigned long long)(Row - MinR),
-                                   (unsigned long long)(Col - MinC), 0});
-      auto It = PinMinLayer.find(Coord);
-      if (It == PinMinLayer.end()) {
-        PinMinLayer.emplace(Coord, Layer);
-      } else {
-        It->second = std::min(It->second, Layer);
-      }
-    }
-  }
-  std::vector<Input::Processed::Route> RouteUnderMinLayer;
-  for (const auto &P : PinMinLayer) {
-    auto RCL = Codec.decode(P.first);
-    unsigned long long R = RCL[0] + MinR;
-    unsigned long long C = RCL[1] + MinC;
-    unsigned long long L = P.second;
-    RouteUnderMinLayer.emplace_back(R, C, L, R, C, MinLayerIdx, Net);
-  }
-  return RouteUnderMinLayer;
-}
 void RoutingGraphManager::createGraph(const std::vector<long long> &LayerFactor,
                                       const std::vector<char> &LayerDir) {
   G.clear();
+  auto &Codec = BondaryInfo->getCodec();
   G.setVertexNum(Codec.max());
 
+  int MinR = BondaryInfo->getMinR();
+  int MinC = BondaryInfo->getMinC();
+  int MinL = BondaryInfo->getMinL();
+
+  int MaxR = BondaryInfo->getMaxR();
+  int MaxC = BondaryInfo->getMaxC();
+  int MaxL = BondaryInfo->getMaxL();
+
   // wire
-  for (int L = MinLayerIdx; L <= MaxLayerIdx; ++L) {
+  for (int L = MinL; L <= MaxL; ++L) {
     for (int R = MinR; R <= MaxR; ++R) {
       for (int C = MinC; C <= MaxC; ++C) {
         if (GridManager->getGrid(R, C, L).getSupply() <= 0)
           continue;
         size_t Coord = Codec.encode({(unsigned long long)(R - MinR),
                                      (unsigned long long)(C - MinC),
-                                     (unsigned long long)(L - MinLayerIdx)});
+                                     (unsigned long long)(L - MinL)});
         if (C != MaxC && GridManager->getGrid(R, C + 1, L).getSupply() > 0 &&
             LayerDir.at(L) == 'H') {
-          size_t NeiCoord =
-              Codec.encode({(unsigned long long)(R - MinR),
-                            (unsigned long long)(C - MinC + 1),
-                            (unsigned long long)(L - MinLayerIdx)});
+          size_t NeiCoord = Codec.encode({(unsigned long long)(R - MinR),
+                                          (unsigned long long)(C - MinC + 1),
+                                          (unsigned long long)(L - MinL)});
           long long Weight = LayerFactor.at(L) * 2;
           G.addEdge(Coord, NeiCoord, Weight);
         }
         if (R != MaxR && GridManager->getGrid(R + 1, C, L).getSupply() > 0 &&
             LayerDir.at(L) == 'V') {
-          size_t NeiCoord =
-              Codec.encode({(unsigned long long)(R - MinR + 1),
-                            (unsigned long long)(C - MinC),
-                            (unsigned long long)(L - MinLayerIdx)});
+          size_t NeiCoord = Codec.encode({(unsigned long long)(R - MinR + 1),
+                                          (unsigned long long)(C - MinC),
+                                          (unsigned long long)(L - MinL)});
           long long Weight = LayerFactor.at(L) * 2;
           G.addEdge(Coord, NeiCoord, Weight);
         }
@@ -127,7 +47,7 @@ void RoutingGraphManager::createGraph(const std::vector<long long> &LayerFactor,
     }
   }
   // via
-  for (int L = MinLayerIdx; L < MaxLayerIdx; ++L) {
+  for (int L = MinL; L < MaxL; ++L) {
     for (int R = MinR; R <= MaxR; ++R) {
       for (int C = MinC; C <= MaxC; ++C) {
         if (GridManager->getGrid(R, C, L).getSupply() <= 0 ||
@@ -135,28 +55,30 @@ void RoutingGraphManager::createGraph(const std::vector<long long> &LayerFactor,
           continue;
         size_t Coord = Codec.encode({(unsigned long long)(R - MinR),
                                      (unsigned long long)(C - MinC),
-                                     (unsigned long long)(L - MinLayerIdx)});
-        size_t NeiCoord = Codec.encode(
-            {(unsigned long long)(R - MinR), (unsigned long long)(C - MinC),
-             (unsigned long long)(L - MinLayerIdx + 1)});
+                                     (unsigned long long)(L - MinL)});
+        size_t NeiCoord = Codec.encode({(unsigned long long)(R - MinR),
+                                        (unsigned long long)(C - MinC),
+                                        (unsigned long long)(L - MinL + 1)});
         long long Weight = LayerFactor.at(L) + LayerFactor.at(L + 1);
         G.addEdge(Coord, NeiCoord, Weight);
       }
     }
   }
 }
-std::vector<Input::Processed::Route> RoutingGraphManager::createFinalRoute(
-    const std::vector<size_t> &Eids,
-    std::vector<Input::Processed::Route> &&RouteUnderMinLayer) {
-  auto Route = std::move(RouteUnderMinLayer);
+std::vector<Input::Processed::Route>
+RoutingGraphManager::createFinalRoute(const std::vector<size_t> &Eids,
+                                      const Input::Processed::Net *Net) {
+  auto Route = std::move(BondaryInfo->getRouteUnderMinLayer());
   for (auto &EdgeIdx : Eids) {
     auto &Edge = G.getEdge(EdgeIdx);
-    auto Decode1 = Codec.decode(Edge.v1);
-    auto Decode2 = Codec.decode(Edge.v2);
+    auto Decode1 = BondaryInfo->getCodec().decode(Edge.v1);
+    auto Decode2 = BondaryInfo->getCodec().decode(Edge.v2);
+    int MinR = BondaryInfo->getMinR();
+    int MinC = BondaryInfo->getMinC();
+    int MinL = BondaryInfo->getMinL();
     unsigned long long R1 = Decode1[0] + MinR, R2 = Decode2[0] + MinR;
     unsigned long long C1 = Decode1[1] + MinC, C2 = Decode2[1] + MinC;
-    unsigned long long L1 = Decode1[2] + MinLayerIdx,
-                       L2 = Decode2[2] + MinLayerIdx;
+    unsigned long long L1 = Decode1[2] + MinL, L2 = Decode2[2] + MinL;
     Route.emplace_back(R1, C1, L1, R2, C2, L2, Net);
   }
   return Route;
@@ -166,17 +88,19 @@ std::pair<std::vector<Input::Processed::Route>, bool>
 GraphApproxRouter::singleNetRoute(
     const Input::Processed::Net *Net,
     const std::vector<Input::Processed::Route> &OriginRoute) {
-
-  RGM.setGraphInfo(getGridManager(), Net, OriginRoute);
-  auto RouteUnderMinLayer =
-      std::move(RGM.createTerminalsAndRouteUnderMinLayer());
+  BondaryBuilder Builder(getGridManager(), Net, OriginRoute);
+  Builder.createBondaryInfo();
+  if (!Builder.isBondaryInfoExist())
+    return {{}, false};
+  auto BondaryInfo = Builder.getBondaryInfo();
+  RGM.setBondaryInfo(&BondaryInfo);
   RGM.createGraph(getLayerFactor(), getLayerDir());
   const auto &G = RGM.getGraph();
   steiner_tree::Solver<long long> Solver(G);
-  auto Res = Solver.solve(RGM.getTerminals());
+  auto Res = Solver.solve(BondaryInfo.getTerminals());
   if (!Res)
     return {{}, false};
-  auto FinalRoute = RGM.createFinalRoute(*Res, std::move(RouteUnderMinLayer));
+  auto FinalRoute = RGM.createFinalRoute(*Res, Net);
   return {FinalRoute, true};
 }
 
